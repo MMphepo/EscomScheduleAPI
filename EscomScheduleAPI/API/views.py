275492a00ec62841495418a36ada3c01c276d9c3
MMphepo <164493503+MMphepo@users.noreply.github.com>
2024-11-14 13:48:01 +0200

    breaking down received data

--- EscomScheduleAPI/API/views.py
+++ EscomScheduleAPI/API/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,11 +9,22 @@ from .serializers import ProgramSerializer
 
 class ProgramView(APIView):
     def post(self, request):
-        serializer = ProgramSerializer(data=request.data.get("data"))
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        json_data = request.data
+        data = json_data["data"]
+        groups = data["groups"]
+        groups = {oldkey.replace(' ', ''): newkey for oldkey, newkey in groups.items()}
+        grpkeys=groups.keys()
+        print(groups)
+        
+        # data_cleaning = Datacleaning()
+        # data_cleaning.clean_spaces(groups)
+        
+        
+        # serializer = ProgramSerializer(data=request.data.get("data"))
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         programs = Program.objects.all()
