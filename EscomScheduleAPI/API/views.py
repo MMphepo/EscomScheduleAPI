7ba7e06015f1beeff37a1b3d57bbd7fbe120f880
MMphepo <164493503+MMphepo@users.noreply.github.com>
2024-11-21 08:36:23 +0200

    receiving data

--- EscomScheduleAPI/API/views.py
+++ EscomScheduleAPI/API/views.py
@@ -3,6 +3,7 @@ import json
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from rest_framework import status
 from .models import Program
 from .serializers import ProgramSerializer
@@ -14,8 +15,39 @@ class ProgramView(APIView):
         groups = data["groups"]
         groups = {oldkey.replace(' ', ''): newkey for oldkey, newkey in groups.items()}
         grpkeys=groups.keys()
-        print(groups)
+        grpids = []
+        regiontypes = []
+        for key in grpkeys:
+            grpid = key[:7] # 'GROUPA2' 
+            regiontype = key[8:] # 'SOUTHERNREGION' 
+            grpids.append(grpid)
+            regiontypes.append(regiontype) 
+        print(grpids)
         
+        affectedAreas = []
+        allAreas = []
+        locations = []
+        # Loop through all groups and print affected areas and locations
+        for id in groups:
+            affectedAarea = groups[id]["affected_areas"]
+            print("-------------------affected areas-----------")
+            print(affectedAarea)
+            affectedAreas.append(affectedAarea)
+            
+            print("------------locations--------------------")
+            location = affectedAarea.keys()
+            locations.append(location)
+            print(location)
+            
+            for lockey in location:
+                areas = affectedAarea[lockey]
+                allAreas.extend(areas)
+                print("all areas of location--", areas)
+        
+        
+        print("absolutely all areas--",allAreas)
+
+            
         # data_cleaning = Datacleaning()
         # data_cleaning.clean_spaces(groups)
         
