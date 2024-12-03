81130b1c60bd30e4bee0203bc347f98d5464f3ad
MMphepo <164493503+MMphepo@users.noreply.github.com>
2024-12-03 13:32:59 +0200

    req

--- EscomScheduleAPI/EscomScheduleAPI/settings.py
+++ EscomScheduleAPI/EscomScheduleAPI/settings.py
@@ -138,5 +138,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "http://127.0.0.1:8000"    # Add more as needed
+    "http://127.0.0.1:8000",
+    "http://localhost:6334"  # Add more as needed
 ]
