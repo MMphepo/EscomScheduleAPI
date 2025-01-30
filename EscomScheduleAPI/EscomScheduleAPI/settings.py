3cd5f7207fd1ae7bcd29009055c8011960d02245
MMphepo <164493503+MMphepo@users.noreply.github.com>
2025-01-30 12:13:11 +0200

    removed localhost cors specification

--- EscomScheduleAPI/EscomScheduleAPI/settings.py
+++ EscomScheduleAPI/EscomScheduleAPI/settings.py
@@ -140,8 +140,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
     "http://127.0.0.1:8000",
     "http://localhost:6334",
-    "https://escom-app-starter.vercel.app" 
-    r"^http://localhost:[0-9]+$",
-    r"^http://127\.0\.0\.1:[0-9]+$",
-# Add more as needed
+    "https://escom-app-starter.vercel.app" # Add more as needed
 ]
