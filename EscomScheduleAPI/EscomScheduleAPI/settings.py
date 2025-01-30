661fdd637e7977916d1eaa7880bd07ba1fd5487b
MMphepo <164493503+MMphepo@users.noreply.github.com>
2025-01-30 10:50:20 +0200

    fixed cors origins

--- EscomScheduleAPI/EscomScheduleAPI/settings.py
+++ EscomScheduleAPI/EscomScheduleAPI/settings.py
@@ -136,9 +136,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://127.0.0.1:5500",
-    # "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     "http://127.0.0.1:8000",
-    # "http://localhost:6334",
-    "https://escom-app-starter.vercel.app" # Add more as needed
+    "http://localhost:6334",
+    "https://escom-app-starter.vercel.app" 
+    r"^http://localhost:[0-9]+$",
+    r"^http://127\.0\.0\.1:[0-9]+$",
+# Add more as needed
 ]
